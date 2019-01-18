970576de03d4f158884030efdbcf0d393996d999
Luke Phyall <lukeprospect@yohoo.com>
2019-01-18 11:49:28 +0000

    array censor first step - split test_tweets

---
+++ array_censor.rb
@@ -0,0 +1,15 @@
+test_tweets = [
+  "This president sucks!",
+  "I hate this Blank House!",
+  "I can't believe we're living with such a bad leadership. We were so foolish",
+  "President Presidentname is a danger to society. I hate that he's so bad – it sucks."
+  ]
+
+banned_phrases = ["sucks", "bad", "hate", "foolish", "danger to society"]
+
+censored_tweets = []
+
+test_tweets.each { |dave|
+  dave.split
+  puts dave
+}
