52cad08a30323c370390763d87a6431ae00b7093
Luke Phyall <lukeprospect@yohoo.com>
2019-01-27 22:18:26 +0000

    Might be on to something wthi a slice/zip combination.

--- playing_with_zip.rb
+++ playing_with_zip.rb
@@ -1,6 +1,15 @@
 values = [1,2,3,4,5,6]
-n = 3
+n = 4
 
 print values.zip(1..n)
 puts ""
 print (1..n*2).zip(values)
+
+puts ""
+
+a = values[0..n-1]
+b = values[n..n+n]
+
+print a
+puts ""
+print b
